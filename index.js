22f6f5e04ff635e03e0606faf37e7fef5a57eb59
krb3p <ryankatherineburke@gmail.com>
2017-03-20 22:38:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much."
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+console.log(couponLocations.reduce(couponCounter, 0));
